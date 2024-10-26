fe3b1ca013ac3b54bcf85414bc0eb5e76c835b01
Vincent-Omondi <vincentomondi251@gmail.com>
2024-10-26 13:56:57 +0300

    Fix: product_helper to handle product authorship and authorization functionality

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,4 +1,9 @@
 module ProductsHelper
   def product_author(product)
+    user_signed_in? && product.user == current_user
+  end
+
+  def seller_name(product)
+    product.user ? product.user.name : "Unknown Seller"
   end
 end
