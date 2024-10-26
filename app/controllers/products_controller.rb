c3cf5ef05f2aac0a87e2ceb54b04453ceba61e68
Vincent-Omondi <vincentomondi251@gmail.com>
2024-10-26 13:55:26 +0300

    fix: Add check_user method for authorization Add before_action :check_user for edit, update, and destroy actions Implement the destroy method Remove unused find_product_by_id method Fix show method to use the already set @product Add proper error handling and redirects

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [ :show, :edit, :update, :destroy ]
-  
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :check_user, only: [:edit, :update, :destroy]
 
   # GET /products or /products.json
   def index
@@ -11,11 +10,6 @@ class ProductsController < ApplicationController
 
   # GET /products/1 or /products/1.json
   def show
-    @products = Product.find(params[:id])
-  end
-
-  def find_product_by_id(id)
-    connection.execute("SELECT * FROM products WHERE products.id = ? LIMIT 1", id).first
   end
 
   # GET /products/new
@@ -27,10 +21,15 @@ class ProductsController < ApplicationController
   def edit
   end
 
+  # Get product by ID
+  def find_product_by_id(id)
+    connection.execute("SELECT * FROM products WHERE products.id = ? LIMIT 1", id).first 
+  end
+
   # POST /products or /products.json
   def create
     @product = current_user.products.build(product_params)
-
+    
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: "Product was successfully created." }
@@ -55,18 +54,27 @@ class ProductsController < ApplicationController
     end
   end
 
- # DELETE /products/1 or /products/1.json
-def destroy
-end
+  # DELETE /products/1 or /products/1.json
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: "Product was successfully deleted." }
+      format.json { head :no_content }
+    end
+  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def product_params
       params.require(:product).permit(:brand, :model, :description, :condition, :finish, :title, :price, :image)
+    end
+
+    def check_user
+      unless @product.user == current_user
+        redirect_to products_path, alert: "Sorry, you're not authorized to perform this action."
+      end
     end
 end
