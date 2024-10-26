d3c742081c58c48b040134ac2313f4a38b44401d
Vincent-Omondi <vincentomondi251@gmail.com>
2024-10-26 23:28:53 +0300

    chore: refactor

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,18 +1,31 @@
 # app/controllers/carts_controller.rb
+# module CurrentCart
+#     private
+#       def set_cart
+#         @cart = Cart.find_by(id: session[:cart_id])
+        
+#         if user_signed_in?
+#           if @cart && @cart.user_id.nil?
+#             # Transfer guest cart to user
+#             @cart.update(user: current_user)
+#           elsif @cart.nil? || @cart.user_id != current_user.id
+#             @cart = Cart.create(user: current_user)
+#           end
+#         elsif @cart.nil?
+#           @cart = Cart.create  # Create cart without user for guests
+#         end
+        
+#         session[:cart_id] = @cart.id
+#       end
+# end
+
 class CartsController < ApplicationController
-    include CurrentCart
-    before_action :set_cart, only: [:show, :destroy]
-    
-    def show
-    end
-    
-    def destroy
-      @cart.destroy if @cart.id == session[:cart_id]
-      session[:cart_id] = nil
-      
-      respond_to do |format|
-        format.html { redirect_to root_path, notice: 'Your cart is now empty!' }
-        format.json { head :no_content }
-      end
-    end
+  def show
+    @cart = Cart.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    logger.error "Attempt to access invalid cart #{params[:id]}"
+    redirect_to root_path, notice: 'Invalid cart'
   end
+
+  # Add other actions as needed (index, destroy, etc.)
+end
