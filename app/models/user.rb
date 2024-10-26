e6cc32c3de9e8a81104b64c8d5432926a37f2ea2
Vincent-Omondi <vincentomondi251@gmail.com>
2024-10-26 14:08:27 +0300

    update: add cart to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :products, dependent: :destroy
-  
+  has_one :cart
   # Add validation for name field
   validates :name, presence: true, length: { maximum: 50 }
 end
