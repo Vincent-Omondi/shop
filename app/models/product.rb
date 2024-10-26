ad88860be9c60242f33a4bfd165b588a3edb345b
Vincent-Omondi <vincentomondi251@gmail.com>
2024-10-26 13:48:05 +0300

    fix: - remove the optional true from belongs_to - ensures data integrity by preventing the deletion of a product that is still referenced by other parts of the application.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,25 @@
 class Product < ApplicationRecord
- 
   before_destroy :not_referenced_by_any_line_item
-  belongs_to :user, optional: true
-
+  belongs_to :user  # Remove optional: true to ensure every product has a user
   mount_uploader :image, ImageUploader
   serialize :image, JSON # If you use SQLite, add this line
 
   validates :title, :brand, :price, :model, presence: true
-  # Set max lenght to the description, price and title 
-  validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum aloud. "}
-  validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum aloud. "}
+  validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed." }  # Fixed typo in "allowed"
+  validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum allowed." }  # Fixed typo in "allowed"
   validates :price, length: { maximum: 10 }
+  validates :user_id, presence: true  # Add this line
 
-  # You can input more brands finish and condition here
-  BRAND = %w{ Ferrari Opel Lenovo Fossil}
+  BRAND = %w{ Ferrari Opel Lenovo Fossil }
   FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
   CONDITION = %w{ New Excellent Mint Used Fair Poor }
 
+  private
+
+  def not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+  end
 end
