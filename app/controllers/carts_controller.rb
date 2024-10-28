5a446c1e1d7b534f89915f89aec3ddf21aa6d404
Vincent-Omondi <vincentomondi251@gmail.com>
2024-10-28 08:25:01 +0300

    fix: create show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,25 +1,8 @@
 # app/controllers/carts_controller.rb
-# module CurrentCart
-#     private
-#       def set_cart
-#         @cart = Cart.find_by(id: session[:cart_id])
-        
-#         if user_signed_in?
-#           if @cart && @cart.user_id.nil?
-#             # Transfer guest cart to user
-#             @cart.update(user: current_user)
-#           elsif @cart.nil? || @cart.user_id != current_user.id
-#             @cart = Cart.create(user: current_user)
-#           end
-#         elsif @cart.nil?
-#           @cart = Cart.create  # Create cart without user for guests
-#         end
-        
-#         session[:cart_id] = @cart.id
-#       end
-# end
-
 class CartsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:show, :destroy, :empty]
+
   def show
     @cart = Cart.find(params[:id])
   rescue ActiveRecord::RecordNotFound
@@ -27,5 +10,15 @@ class CartsController < ApplicationController
     redirect_to root_path, notice: 'Invalid cart'
   end
 
-  # Add other actions as needed (index, destroy, etc.)
+  def destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
+    redirect_to root_path, notice: 'Your cart is currently empty'
+  end
+
+  def empty
+    @cart.line_items.destroy_all
+    redirect_to @cart, notice: 'Cart emptied successfully'
+  end
+
 end
