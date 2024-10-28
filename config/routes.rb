ebe4e3fe87ec3103860aa9fdb760a25f2e5cb591
Vincent-Omondi <vincentomondi251@gmail.com>
2024-10-28 20:57:48 +0300

    fix;  defined the empty_cart_path route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  resources :carts do
+    member do
+      delete :empty
+    end
+  end
+  
   resources :products
   resources :line_items
-  resources :carts
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
   root 'products#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
