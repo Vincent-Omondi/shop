eb93a3a263b79b43756743b9d9b117f3c398d22b
Vincent-Omondi <vincentomondi251@gmail.com>
2024-10-28 23:21:49 +0300

    feat: add product title in the action messages

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,59 +1,44 @@
 # app/controllers/line_items_controller.rb
-# class LineItemsController < ApplicationController
-#     include CurrentCart
-#     before_action :set_cart, only: [:create]
-#     before_action :set_line_item, only: [:destroy]
-    
-#     def create
-#       product = Product.find(params[:product_id])
-#       @line_item = @cart.add_product(product)
-      
-#       respond_to do |format|
-#         if @line_item.save
-#           format.html { redirect_back(fallback_location: root_path, notice: 'Added to your cart') }
-#           format.js   { @current_item = @line_item }
-#           format.json { render :show, status: :created, location: @line_item }
-#         else
-#           format.html { render :new }
-#           format.json { render json: @line_item.errors, status: :unprocessable_entity }
-#         end
-#       end
-#     end
-    
-#     def destroy
-#       @cart = Cart.find(session[:cart_id])
-#       @line_item.destroy
-      
-#       respond_to do |format|
-#         format.html { redirect_to cart_path(@cart), notice: 'Removed from your cart' }
-#         format.json { head :no_content }
-#       end
-#     end
-    
-#     private
-#       def set_line_item
-#         @line_item = LineItem.find(params[:id])
-#       end
-#   end
-
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create, :destroy]
+  before_action :set_cart
+  before_action :set_line_item, only: [:destroy, :update_quantity]
 
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
 
     if @line_item.save
-      redirect_to @line_item.cart, notice: 'Item added to your cart'
+      redirect_to @line_item.cart, notice: "#{product.title} added to your cart"
     else
-      redirect_to product, alert: 'Failed to add item to cart'
+      redirect_to product, alert: "Failed to add #{product.title} to cart"
     end
   end
 
   def destroy
-    @line_item = LineItem.find(params[:id])
+    product_title = @line_item.product.title
     @line_item.destroy
-    redirect_to @cart, notice: 'Item removed from your cart'
+    redirect_to @cart, notice: "#{product_title} removed from your cart"
+  end
+
+  def update_quantity
+    product_title = @line_item.product.title
+    if params[:change] == 'increase'
+      @line_item.quantity += 1
+    elsif params[:change] == 'decrease' && @line_item.quantity > 1
+      @line_item.quantity -= 1
+    end
+
+    if @line_item.save
+      redirect_to @cart, notice: "#{product_title} quantity updated"
+    else
+      redirect_to @cart, alert: "Error updating #{product_title} quantity"
+    end
+  end
+
+  private
+
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
   end
 end
