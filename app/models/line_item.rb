83acf2d3512099dcfcdf7f0b635183381a0e8e47
Vincent-Omondi <vincentomondi251@gmail.com>
2024-10-28 20:53:28 +0300

    add; a default quantity to the LineItem model

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,9 +1,18 @@
 # app/models/line_item.rb
 class LineItem < ApplicationRecord
-    belongs_to :cart
-    belongs_to :product
-     
-    def total_price
-      product.price * quantity
-    end
+  belongs_to :cart
+  belongs_to :product
+  
+  # Add default value for quantity
+  before_create :set_default_quantity
+  
+  def total_price
+    product.price * (quantity || 1)  # Fallback to 1 if quantity is nil
   end
+
+  private
+
+  def set_default_quantity
+    self.quantity ||= 1
+  end
+end
