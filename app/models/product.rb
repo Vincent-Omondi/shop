c5b008f34713b11488e6eba53a5f6be2ae95e8c8
Vincent-Omondi <vincentomondi251@gmail.com>
2024-11-03 13:30:55 +0300

    feat: add file validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,24 @@
 class Product < ApplicationRecord
   before_destroy :not_referenced_by_any_line_item
-  belongs_to :user  # Remove optional: true to ensure every product has a user
+  belongs_to :user
   mount_uploader :image, ImageUploader
   has_many :line_items
-  serialize :image, JSON # If you use SQLite, add this line
-
-  # validates :brand, :model, :description, :condition, :finish, :title, presence: true
-  validate :all_fields_present
-  validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed." }  # Fixed typo in "allowed"
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum allowed." }  # Fixed typo in "allowed"
-  validates :price, length: { maximum: 10 }
-  validates :user_id, presence: true 
+  serialize :image, JSON # For SQLite
 
   BRAND = %w{ Ferrari Opel Lenovo Fossil }
   FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
   CONDITION = %w{ New Excellent Mint Used Fair Poor }
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :description, presence: true, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed." }
+  validates :brand, presence: true
+  validates :finish, presence: true
+  validates :condition, presence: true
+  validates :image, presence: true
+  validates :user_id, presence: true
+  validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum allowed." }
+  validates :price, length: { maximum: 10 }
 
   private
 
@@ -25,8 +28,9 @@ class Product < ApplicationRecord
       throw :abort
     end
   end
+
   def all_fields_present
-    required_fields = [brand, model, description, condition, finish, title, price, image]
+    required_fields = [brand, description, condition, finish, title, price, image]
     if required_fields.any?(&:blank?)
       errors.add(:base, "Please fill in all required fields and attach an image")
     end
