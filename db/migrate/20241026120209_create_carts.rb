4c8a62de9b602e1ba32dd0f1687b132a19e3d73c
Vincent-Omondi <vincentomondi251@gmail.com>
2024-11-24 18:58:44 +0300

    chore: update migrations

--- db/migrate/20241026120209_create_carts.rb
+++ db/migrate/20241026120209_create_carts.rb
@@ -1,4 +1,4 @@
-class CreateCarts < ActiveRecord::Migration[7.0]
+class CreateCarts < ActiveRecord::Migration[6.1]
   def change
     create_table :carts do |t|
       t.references :user, foreign_key: true, null: true  # Explicitly set null: true
--- db/migrate/20241026122817_add_default_quantity_to_line_items.rb
+++ db/migrate/20241026122817_add_default_quantity_to_line_items.rb
@@ -1,4 +1,4 @@
-class AddDefaultQuantityToLineItems < ActiveRecord::Migration[7.0]
+class AddDefaultQuantityToLineItems < ActiveRecord::Migration[6.1]
   def change
     change_column_default :line_items, :quantity, from: nil, to: 1
   end
