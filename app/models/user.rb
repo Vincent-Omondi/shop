11d09586b1e3bd495ec5c5253ee215a51a5879c5
vinomondi <vincentomondi1002@gmail.com>
2025-01-29 11:55:02 +0300

    fix: user deletion with associated cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :products, dependent: :destroy
-  has_one :cart
+  has_one :cart, dependent: :destroy
   # Add validation for name field
   validates :name, presence: true, length: { maximum: 50 }
 end
